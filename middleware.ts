a4b94f8eb2d875b6ec612c26e8a2b78a90dcb506
Mark Terrious <named1006@gmail.com>
2024-04-12 17:36:48 +0900

    matcher fix

--- middleware.ts
+++ middleware.ts
@@ -2,21 +2,9 @@ import { NextRequest, NextResponse } from "next/server";
 import { getSession } from "./lib/session";
 
 export async function middleware(request: NextRequest) {
-    const pathname = request.nextUrl.pathname;
-   
-    if (pathname == "/") {
-        // response 반환
-        const res = NextResponse.next();
-        res.cookies.set("middleware-cookie", "hello");
-        return res;
-    }
-    if (pathname == "/profile") {
-        return Response.redirect(new URL("/", request.url));
-    }
-
-    console.log("hello? d");
+      console.log("hello? d");
 }
 
 export const config = {
-    matcher: ["/", "/profile", "/create-account"],
+    matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"]
 }
